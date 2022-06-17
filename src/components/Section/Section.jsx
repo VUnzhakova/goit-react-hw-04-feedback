fd8cc99d57362c2f7acfb0bd6c92b4fd7d883f01
Valentina Unzhakova <vunzhakova@outlook.com>
2022-06-17 22:55:23 +0300

    section

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,18 +1,18 @@
-// import s from './Section.module.css';
-// import PropTypes from 'prop-types';
+import s from './Section.module.css';
+import PropTypes from 'prop-types';
 
-// function Section({ title, children }) {
-//   return (
-//     <section className={s.section}>
-//       <h2 className={s.title}>{title}</h2>
-//       {children}
-//     </section>
-//   );
-// }
+function Section({ title, children }) {
+  return (
+    <section className={s.section}>
+      <h2 className={s.title}>{title}</h2>
+      {children}
+    </section>
+  );
+}
 
-// Section.propTypes = {
-//   title: PropTypes.string.isRequired,
-//   children: PropTypes.element,
-// };
+Section.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.element,
+};
 
-// export default Section;
+export default Section;
