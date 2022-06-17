14a381dc7b6bfc0e105675724320c73be7d524b1
Valentina Unzhakova <vunzhakova@outlook.com>
2022-06-17 22:57:38 +0300

    options

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,26 +1,26 @@
-// import s from './FeedbackOptions.module.css';
-// import PropTypes from 'prop-types';
+import s from './FeedbackOptions.module.css';
+import PropTypes from 'prop-types';
 
-// function FeedbackOptions({ options, onLeaveFeedback }) {
-//   return (
-//     <>
-//       <div>
-//         {options.map(option => {
-//           return (
-//             <button
-//               type="button"
-//               onClick={() => onLeaveFeedback(option)}
-//               key={option}
-//               className={s.btn}
-//             >
-//               {option}
-//             </button>
-//           );
-//         })}
-//       </div>
-//     </>
-//   );
-// }
+function FeedbackOptions({ options, onLeaveFeedback }) {
+  return (
+    <>
+      <div>
+        {options.map(option => {
+          return (
+            <button
+              type="button"
+              onClick={() => onLeaveFeedback(option)}
+              key={option}
+              className={s.btn}
+            >
+              {option}
+            </button>
+          );
+        })}
+      </div>
+    </>
+  );
+}
 
 FeedbackOptions.propTypes = {
   options: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
