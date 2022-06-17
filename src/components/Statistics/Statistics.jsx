0931b353329d55a87f7b8baedb47521b0613b4a2
Valentina Unzhakova <vunzhakova@outlook.com>
2022-06-17 22:54:52 +0300

    Statistics

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,24 +1,24 @@
-// import s from './Statistics.module.css';
-// import PropTypes from 'prop-types';
+import s from './Statistics.module.css';
+import PropTypes from 'prop-types';
 
-// function Statistics({ good, neutral, bad, total, positivePercentage }) {
-//   return (
-//     <ul className={s.list}>
-//       <li className={s.item}>Good: {good}</li>
-//       <li className={s.item}>Neutral: {neutral}</li>
-//       <li className={s.item}>Bad: {bad}</li>
-//       <li className={s.item}>Total: {total}</li>
-//       <li className={s.item}>Positive feedback: {positivePercentage}%</li>
-//     </ul>
-//   );
-// }
+function Statistics({ good, neutral, bad, total, positivePercentage }) {
+  return (
+    <ul className={s.list}>
+      <li className={s.item}>Good: {good}</li>
+      <li className={s.item}>Neutral: {neutral}</li>
+      <li className={s.item}>Bad: {bad}</li>
+      <li className={s.item}>Total: {total}</li>
+      <li className={s.item}>Positive feedback: {positivePercentage}%</li>
+    </ul>
+  );
+}
 
-// Statistics.propTypes = {
-//   good: PropTypes.number.isRequired,
-//   neutral: PropTypes.number.isRequired,
-//   bad: PropTypes.number.isRequired,
-//   total: PropTypes.number.isRequired,
-//   positivePercentage: PropTypes.number.isRequired,
-// };
+Statistics.propTypes = {
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  positivePercentage: PropTypes.number.isRequired,
+};
 
-// export default Statistics;
+export default Statistics;
